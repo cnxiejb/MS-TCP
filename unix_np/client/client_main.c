671662325bf04404bcc1f110721fcee43b044574
xiejinbao <xiejinbao@corp.netease.com>
2016-11-14 17:49:47 +0800

    signal waitpid

--- unix_np/client/client_main.c
+++ unix_np/client/client_main.c
@@ -1,7 +1,7 @@
 /*************************************************************************
 	> File Name: client_main.c
-	> Author: 
-	> Mail: 
+	> Author:
+	> Mail:
 	> Created Time: 四 11/10 15:13:22 2016
  ************************************************************************/
 #include "child_process.h"
@@ -11,25 +11,26 @@
 int main(int argc,char **argv)
 {
     struct sockaddr_in servaddr;
-    int sockfd;
+    int i,sockfd,sockf[5];
 
-    sockfd = socket(AF_INET,SOCK_STREAM,0);
-    
-    bzero(&servaddr,sizeof(servaddr));
-    servaddr.sin_family = AF_INET;
-    servaddr.sin_port = htons(SERV_PORT);
-    if(inet_pton(AF_INET,argv[1],&servaddr.sin_addr) <= 0)
+    for(i =0; i < 5; i++)
     {
-        perror("inet_pton");
-        return -1;
+        sockf[i] = socket(AF_INET,SOCK_STREAM,0);
+        bzero(&servaddr,sizeof(servaddr));
+        servaddr.sin_family = AF_INET;
+        servaddr.sin_port = htons(SERV_PORT);
+        if(inet_pton(AF_INET,argv[1],&servaddr.sin_addr) <= 0)
+        {
+            perror("inet_pton");
+            return -1;
+        }
+        if(connect(sockf[i],(struct sockaddr *)&servaddr,sizeof(servaddr)) < 0)
+        {
+            perror("connect");
+            return -2;
+        }
     }
 
-    if(connect(sockfd,(struct sockaddr *)&servaddr,sizeof(servaddr)) < 0)
-    {
-        perror("connect");
-        return -2;
-    }
-    
-    str_cli(stdin,sockfd);
+    str_cli(stdin,sockf[0]);
     exit(0);
 }
--- unix_np/server/sys_signal.c
+++ unix_np/server/sys_signal.c
@@ -25,7 +25,8 @@ void sig_child(int signo)
     pid_t pid;
     int stat;
 
-    pid = wait(&stat);
-    printf("child %d terminated\n",pid);
+    while((pid = waitpid(-1,&stat,WNOHANG)) > 0)
+        printf("child %d terminated\n",pid);
+
     return;
 }
